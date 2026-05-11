7c5f88117368bed383b01547a7a7f85dae0979e8
oliverdann7 <danezolv@gmail.com>
2026-05-11 19:32:07 +0300

    Use manual Express handler wrapper for Vercel serverless

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,11 @@
 import { createApiApp } from '../server';
 
+if (!process.env.GEMINI_API_KEY) {
+  console.error('GEMINI_API_KEY is not set');
+}
+
 const app = createApiApp();
 
-export default app;
+export default function handler(req: any, res: any) {
+  app(req, res);
+}
